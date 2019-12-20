f78e95dcf508d79d51d4c76260e06d2bf429cf08
atvd <iuhthanhtung4498@gmail.com>
2019-12-20 12:00:36 +0700

    add domain entity for project

---
+++ ShopOnline.Model/Models/Footer.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ShopOnline.Model.Models
+{
+    [Table("Footers")]
+    public class Footer
+    {
+        [Key]
+        public string ID { set; get; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Content { set; get; }
+    }
+}
